2eb9b29b5c0669269fa8202b86964188e05c279d
HugoHsp <81809906+HugoHsp@users.noreply.github.com>
2021-04-02 13:35:09 +0200

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,18 @@
+console.log("hello mec");
+console.log("-------------");
+
+let name = "john";
+console.log(name);
+console.log("-------------");
+
+let x = 12 ;
+console.log(x);
+
+let z=parseInt(prompt("saisir un chiffre"));
+let r=parseInt(prompt("saisir un autre chiffre"));
+for (i=1 ; i<10 ; i++)
+	{ 
+	z=z*r+r; 
+	}
+console.log(z);
+
